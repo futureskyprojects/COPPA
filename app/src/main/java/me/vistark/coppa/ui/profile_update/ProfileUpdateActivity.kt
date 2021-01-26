7e1025857847fbdec09c8c90c93a26bdad98e6fc
Vistark <services.vistark@gmail.com>
2021-01-26 17:32:29 +0700

    Update

---
+++ app/src/main/java/me/vistark/coppa/ui/profile_update/ProfileUpdateActivity.kt
@@ -0,0 +1,11 @@
+package me.vistark.coppa.ui.profile_update
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_update)
+    }
+}
